ccd102a2973d01fa62e773bce691dbe0a9ac861c
Tawanda Kanyangarara <-g>
2020-05-23 18:52:56 +0100

    wrench aka build

---
+++ src/app/components/material-icons/animat-wrench/animat-wrench.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'animat-wrench',
+  templateUrl: './animat-wrench.component.html',
+  styleUrls: ['./animat-wrench.component.scss']
+})
+export class AnimatWrenchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
