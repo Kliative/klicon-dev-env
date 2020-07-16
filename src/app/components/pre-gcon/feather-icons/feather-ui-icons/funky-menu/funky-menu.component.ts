71845bb0794fb8b24079fccd3c8bbc0c97111856
Tawanda Kanyangarara <-g>
2020-07-16 15:28:03 +0100

    Ui complete

--- src/app/components/pre-gcon/feather-icons/feather-ui-icons/funky-menu/funky-menu.component.ts
+++ src/app/components/pre-gcon/feather-icons/feather-ui-icons/funky-menu/funky-menu.component.ts
@@ -1,12 +1,11 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { BaseComponent } from 'src/app/components/base/base.component';
+import { BaseComponent } from 'src/app/garicons/components/base/base.component';
+
 
 @Component({
   selector: 'garicon-funky-menu',
   templateUrl: './funky-menu.component.html',
   styleUrls: ['./funky-menu.component.scss']
 })
-export class FunkyMenuComponent extends BaseComponent implements OnInit {
-  
+export class FunkyMenuComponent extends BaseComponent {}
 
-}
