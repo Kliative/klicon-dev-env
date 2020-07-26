55584e016af5b63548f18a04091fd3a9e973e054
Tawanda Kanyangarara <-g>
2020-07-26 18:11:09 +0100

    align left + right + justify

--- src/app/garicons/components/align/align.component.ts
+++ src/app/garicons/components/align/align.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { BaseComponent } from '../base/base.component';
 
 @Component({
@@ -7,10 +7,24 @@ import { BaseComponent } from '../base/base.component';
   styleUrls: ['./align.component.scss']
 })
 export class AlignComponent extends BaseComponent {
+  align: string;
+
+  @Input('align')
+  set setAlign(align: string) {
+    this.align = align;
+  }
+
+  get getAlign(): string {
+    return this.align;
+  }
 
   constructor() {
     super();
+    this.align = this.initialiseAlignment(this.align);
   }
 
+  initialiseAlignment(align: string): string {
+    return align ? align : 'justify';
+  }
 
 }
